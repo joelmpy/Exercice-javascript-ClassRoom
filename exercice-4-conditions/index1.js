87f441da6b96fc143661b557839b36d6f4b808f2
Joel <joel_mpy@hotmail.com>
2022-06-14 22:42:42 +0200

    commit exercice-4

--- exercice-4-conditions/index1.js
+++ exercice-4-conditions/index1.js
@@ -1,52 +1,51 @@
-let user = false;
-let message = "";
+// let user = false;
+// let message = "";
 
-if(user){
-    message = "Welcome my new channel";
-}else {
-    message = "Welcome to home";
-}
+// if (user) {
+//   message = "Welcome my new channel";
+// } else {
+//   message = "Welcome to home";
+// }
 
-console.log(message);
 
 let nombre1 = 143;
 let nombre2 = 219;
-let texte = ""
-if(nombre1 > nombre2){
-  document.querySelector('#number-1').innerHTML = "bonjour"
- console.log(texte.innerHTML = "Vous etes pas rentrer") 
-}else{
-  document.querySelector('#number-1').innerHTML = "bonjour a tous"
-  console.log(texte.innerHTML = "Vous etes rentrer")
+let texte = "";
+if (nombre1 > nombre2) {
+  document.querySelector("#number-1").innerHTML = "bonjour";
+  console.log((texte.innerHTML = "Vous etes pas rentrer"));
+} else {
+  document.querySelector("#number-1").innerHTML = "bonjour a tous";
+  console.log((texte.innerHTML = "Vous etes rentrer"));
 }
-
 
 class Utilsateur {
-    constructor(name, age, accountLevel){
-        this.name = name,
-        this.age = age,
-        this.accountLevel = accountLevel
-    }
+  constructor(name, age, accountLevel) {
+    (this.name = name), (this.age = age), (this.accountLevel = accountLevel);
+  }
 }
 
-let firstUser = new Utilsateur('Will Alexander', 25, 'normal');
-let secondUser = new Utilsateur('Jojo Mani', 35, 'premium');
-let threeUser = new Utilsateur('Micheal John', 15, 'mega-premium');
+let firstUser = new Utilsateur("Will Alexander", 25, "normal");
+let secondUser = new Utilsateur("Jojo Mani", 35, "premium");
+let threeUser = new Utilsateur("Micheal John", 15, "mega-premium");
 
 let utilsateurs = [firstUser, secondUser, threeUser];
 
-if (firstUser.accountLevel === 'normal'){
-    console.log('Ils sont normal')
-}else {
-    console.log('ils sont pas normal du tout')
+if (firstUser.accountLevel === "normal") {
+  console.log("Ils sont normal");
+} else {
+  console.log("ils sont pas normal du tout");
 }
 
-switch(secondUser.accountLevel) {
-    case 'normal' : console.log('ils sont normal')
+switch (secondUser.accountLevel) {
+  case "normal":
+    console.log("ils sont normal");
     break;
 
-    case 'premium' : console.log('Ils sont premium');
+  case "premium":
+    console.log("Ils sont premium");
     break;
 
-    case 'mega-premium' : console.log('Ils sont mega premium');
+  case "mega-premium":
+    console.log("Ils sont mega premium");
 }
