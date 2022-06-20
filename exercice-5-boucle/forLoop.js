988c1b42ed455d4cd6f8aaec432c4fe1bb7989d5
Joel <joel_mpy@hotmail.com>
2022-06-20 17:47:31 +0200

    boucle

--- exercice-5-boucle/forLoop.js
+++ exercice-5-boucle/forLoop.js
@@ -40,3 +40,81 @@ for (var i = 12; i < 14; i++) {
 console.log(box1);
 console.log(box2);
 console.log(i);
+
+
+var nombres = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048];
+
+// TODO: ÉTAPE 1
+// À l'aide d'une boucle for, afficher dans la console toutes les valeurs
+// contenues dans le tableau `nombres`, une par une.
+
+for (let i = 0; i < nombres.length; i++) {
+  console.log(nombres[i]);
+}
+
+// TODO: ÉTAPE 2
+// Avec une autre boucle for, afficher dans la console toutes les valeurs
+// du tableau `nombres`, une par une, sauf les deux premières.
+for (let i = 2; i < nombres.length; i++) {
+  console.log(nombres[i]);
+}
+
+// TODO: ÉTAPE 3
+// Avec une autre boucle for, afficher dans la console toutes les valeurs
+// du tableau `nombres`, une par une, sauf les trois dernières.
+
+for (let i = 0; i < nombres.length - 3; i++) {
+  console.log(nombres[i])
+}
+
+// TODO: ÉTAPE 4
+// Avec une autre boucle for, afficher dans la console toutes les valeurs
+// du tableau `nombres`, une par une, mais en commençant par la dernière
+// valeur et en terminant par la première.
+
+for (let i = nombres.length - 1; i >= 0; i--) {
+    console.log(nombres[i]);
+}
+
+// TODO: ÉTAPE 5
+// Avec une autre boucle for, afficher dans la console uniquement les
+// valeurs du tableau `nombres` ayant un indice pair (0, 2, 4, etc.),
+// une par une.
+
+for (let i = 0; i < nombres.length; i++) {
+  if (2 % i === 0) {
+    console.log('ils sont pair' + ' ---> ', nombres[i]);
+  } else {
+    console.log('ils sont pas pair', nombres[i]);
+  }
+}
+
+var prenoms = ['Ali', 'Nour', 'Jean', 'Carole', 'Ibrahim', 'Alma'];
+
+// TODO: ÉTAPE 6
+// À l'aide d'une boucle for, afficher dans la console des messages
+// qui saluent chaque prénom contenus dans le tableau `prenoms`.
+// Par exemple : "Bonjour Joseph"
+
+let lengthPrenoms = prenoms.length;
+
+for (let i = 0; i < lengthPrenoms; i++) {
+//   console.log('Bonjour', prenoms[i]);
+}
+
+// TODO: ÉTAPE 7
+// Avec une autre boucle for, afficher dans la console uniquement
+// le premier prénom contenant plus de 4 lettres.
+
+for (let i = 0; i < lengthPrenoms; i++) {
+  let groupe = prenoms[i];
+  if (groupe.length > 4) {
+    console.log(groupe);
+  }
+}
+
+let text = ""
+for (let i = 0; i < lengthPrenoms; i++) {
+    text = text + prenoms[i] + ","
+    console.log(text);
+  }
