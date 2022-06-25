f1ac8cd642772b0a73b7fea9047450b428eca121
Joel <joel_mpy@hotmail.com>
2022-06-25 14:43:09 +0200

    Dom-jour-1

--- exercice-Dom-js/DOM-jour-1/main.js
+++ exercice-Dom-js/DOM-jour-1/main.js
@@ -5,49 +5,57 @@ const liItem = document.querySelector("article ul.important > li");
 let mainParent = document.getElementById("main");
 mainParent.innerHTML = "<ul><li>Jour 1</li><li>Jour 2</li></ul>";
 
-const newDiv = document.createElement('nav')
-mainParent.appendChild(newDiv).innerHTML = "<ul><li>Bonjour</li></ul>"
+const newDiv = document.createElement("nav");
+mainParent.appendChild(newDiv).innerHTML = "<ul><li>Bonjour</li></ul>";
 newDiv.style.backgroundColor = "black";
 newDiv.style.textAlign = "center";
-newDiv.style.color = "white"
+newDiv.style.color = "white";
 
-console.log(mainParent)
+console.log(mainParent);
 
 // exercice create element//
 
-const newParagraphe = document.createElement('p');
+const newParagraphe = document.createElement("p");
 mainParent.appendChild(newParagraphe);
-newParagraphe.innerHTML = "Mon <strong>grand</strong> contenu"
-newParagraphe.classList.add('important');
-newParagraphe.style.color = 'green'
+newParagraphe.innerHTML = "Mon <strong>grand</strong> contenu";
+newParagraphe.classList.add("important");
+newParagraphe.style.color = "green";
 
 // exercice evenement //
 
-let button = document.querySelectorAll('#clock')
-for (let i = 0; i < button.length; i++){
-    let allButton = button[i];
-    allButton.addEventListener('click', function () {
-       allButton.style.display = "none"
-    })
+let button = document.querySelectorAll("#clock");
+for (let i = 0; i < button.length; i++) {
+  let allButton = button[i];
+  allButton.addEventListener("click", function () {
+    allButton.style.display = "none";
+  });
 }
 
-let parent = document.getElementById('parent');
-let child = document.getElementById('child');
-let countParent = document.getElementById('parent-count');
-let countChild = document.getElementById('child-count')
+let parent = document.getElementById("parent");
+let child = document.getElementById("child");
+let countParent = document.getElementById("parent-count");
+let countChild = document.getElementById("child-count");
 
-let countP = 0
-let countC = 0
-parent.addEventListener('click', () => {
-   countParent.innerHTML = (countP++) + ""
-})
+let countP = 0;
+let countC = 0;
+parent.addEventListener("click", () => {
+  countParent.innerHTML = countP++ + "";
+});
 
-child.addEventListener('click', (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    countChild.innerHTML = (countC++) + ""
-})
+child.addEventListener("click", (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+  countChild.innerHTML = countC++ + "";
+});
 
+let selctChange = document.querySelector("#ice-cream");
 
+selctChange.addEventListener("change", (event) => {
+  let result = document.querySelector(".result");
+  if(!event.target.value){
+    alert('please selct option')
+  }else{
+    result.textContent = `Tu aime bien ${event.target.value}`;
+  }
 
-
+});
