5e3c856194f661df9b8fed7c331c64e221903f59
Joel <joel_mpy@hotmail.com>
2022-06-27 11:46:34 +0200

    DOM-jour-2

--- exercice-Dom-js/DOM-jour-2/main.js
+++ exercice-Dom-js/DOM-jour-2/main.js
@@ -3,15 +3,14 @@
 let selctChange = document.querySelector("#ice-cream");
 
 selctChange.addEventListener("change", (event) => {
-  let result = document.querySelector(".result");
+  let resultat = document.querySelector(".resultat");
   if(!event.target.value){
     alert('please selct option')
   }else{
-    result.textContent = `Tu aime bien ${event.target.value}`;
+    resultat.textContent = `Tu aime bien ${event.target.value}`;
   }
 
 });
-
 
 
 // Exercice 
@@ -21,4 +20,35 @@ let input = document.getElementById('input');
 input.addEventListener('input', (event) => {
   let lettre = document.getElementById('lettre');
   lettre.innerHTML = event.target.value
+})
+
+// Exercice Mousevent // 
+
+let inputName = document.getElementById('name')
+let gender = document.getElementById('gender');
+
+inputName.addEventListener('input', (event) => {
+  let resName = document.getElementById('res-name');
+  resName.innerHTML = event.target.value
+});
+
+gender.addEventListener('change', (event) => {
+  let resGender = document.getElementById('res-gender');
+  if(!event.target.value){
+    alert('Tu dois selectionne un choix')
+  }
+  else {
+    resGender.innerHTML = event.target.value
+  }
+})
+
+let result = document.getElementById('result');
+let x = document.getElementById('mouse-x');
+let y = document.getElementById('mouse-y');
+
+result.addEventListener('mousemove', (event) => {
+  const offsetX = event.offsetX
+  const offsetY = event.offsetY
+  x.innerHTML = offsetX
+  y.innerHTML = offsetY
 })
