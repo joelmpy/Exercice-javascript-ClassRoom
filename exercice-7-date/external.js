6d49b1b8f517fcc96f0d057680221dace4187917
Joel <joel_mpy@hotmail.com>
2022-07-01 13:23:10 +0200

    test

---
+++ exercice-7-date/external.js
@@ -0,0 +1,17 @@
+function hello (){
+    return "bonjour"
+}
+
+function helloEverybody () {
+    return "bonjour tout le monde"
+}
+
+const groupe = ['blue', 'green', 'red', 'black', 'yelow'];
+
+module.exports = {
+    hello, 
+    helloEverybody,
+    groupe
+}
+
+
---
+++ exercice-7-date/index.js
@@ -0,0 +1,5 @@
+var exemple = require('./external')
+
+console.log(exemple.hello())
+console.log(exemple.helloEverybody())
+console.log(exemple.groupe)
